4121dd59e852c5f8da74598d8feea4d6fa98ef48
Aaron Dinesh <aarondinesh2002@gmail.com>
2024-02-15 15:42:41 +0000

    Removed the need for the user to declare the types in the loading functions. Type is inferred now from scan

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -39,7 +39,7 @@ function domain_hemisphere(resolution::Real, radius::Real, dtype)
     return stack((points[1], points[2], points[3]); dims=2), (-dtype(radius):dtype(resolution):dtype(radius), -dtype(radius):dtype(resolution):dtype(radius), dtype(0):dtype(resolution):dtype(radius))
 end
 
-function domain_hemisphere!(scan::BreastScan{<:AbstractFloat, <:Number, <:Integer}, resolution::Real, radius::Real, dtype::Type)
+function domain_hemisphere!(scan::BreastScan{T, Y, Z}, resolution::Real, radius::Real) where {T <: Real, Y <: Number, Z <:Integer}
     if radius <= 0.0
         throw(DomainError(radius, "The radius cannot be 0 or negative"))
     end
@@ -52,12 +52,12 @@ function domain_hemisphere!(scan::BreastScan{<:AbstractFloat, <:Number, <:Intege
         error("The resolution cannot be greater than 2*radius")
     end
 
-    points = Vector[dtype[], dtype[], dtype[]]
+    points = Vector[T[], T[], T[]]
 
     #Check the numbers of 1s in the matlab
-    for x in -dtype(radius):dtype(resolution):dtype(radius)
-        for y in -dtype(radius):dtype(resolution):dtype(radius)
-            for z in dtype(0):dtype(resolution):dtype(radius)
+    for x in -T(radius):T(resolution):T(radius)
+        for y in -T(radius):T(resolution):T(radius)
+            for z in T(0):T(resolution):T(radius)
                 if(x^2 + y^2 + z^2 <= radius^2)
                     push!(points[1], x)
                     push!(points[2], y)
@@ -68,25 +68,25 @@ function domain_hemisphere!(scan::BreastScan{<:AbstractFloat, <:Number, <:Intege
     end
 
     scan.points = stack((points[1], points[2], points[3]); dims=2)
-    scan.axes = (-dtype(radius):dtype(resolution):dtype(radius), -dtype(radius):dtype(resolution):dtype(radius), dtype(0):dtype(resolution):dtype(radius))
+    scan.axes = (-T(radius):T(resolution):T(radius), -T(radius):T(resolution):T(radius), T(0):T(resolution):T(radius))
 end
 
-function load_scans!(scan::BreastScan{<:AbstractFloat, <:Number}, source1::String, delim::AbstractChar, T::Type)
-    scan.signal = readdlm(source1, delim, T, use_mmap=true)
+function load_scans!(scan::BreastScan{T, Y, Z}, source1::String, delim::AbstractChar) where {T <: Real, Y <: Number, Z <:Integer}
+    scan.signal = readdlm(source1, delim, Y, use_mmap=true)
 end
 
-function load_scans!(scan::BreastScan{<:AbstractFloat, <:Number}, source1::String, source2::String, delim::AbstractChar, T::Type)
-    scan.signal = readdlm(source1, delim, T, use_mmap=true) .- readdlm(source2, delim, T, use_mmap=true)
+function load_scans!(scan::BreastScan{T, Y, Z}, source1::String, source2::String, delim::AbstractChar) where {T <: Real, Y <: Number, Z <:Integer}
+    scan.signal = readdlm(source1, delim, Y, use_mmap=true) .- readdlm(source2, delim, Y, use_mmap=true)
 end
 
-function load_frequencies!(scan::BreastScan{<:AbstractFloat, <:Number}, source1::String, delim::AbstractChar, T::Type)
+function load_frequencies!(scan::BreastScan{T, Y, Z}, source1::String, delim::AbstractChar) where {T <: Real, Y <: Number, Z <:Integer}
     scan.frequencies = readdlm(source1, delim, T, use_mmap=true)
 end
 
-function load_antennas!(scan::BreastScan{<:AbstractFloat, <:Number}, source1::String, delim::AbstractChar, T::Type)
+function load_antennas!(scan::BreastScan{T, Y, Z}, source1::String, delim::AbstractChar) where {T <: Real, Y <: Number, Z <:Integer}
     scan.antennas = readdlm(source1, delim, T, use_mmap=true)
 end
 
-function load_channels!(scan::BreastScan{<:AbstractFloat, <:Number}, source1::String, delim::AbstractChar, T::Type)
-    scan.channels = readdlm(source1, delim, T, use_mmap=true)
+function load_channels!(scan::BreastScan{T, Y, Z}, source1::String, delim::AbstractChar) where {T <: Real, Y <: Number, Z <:Integer}
+    scan.channels = readdlm(source1, delim, Z, use_mmap=true)
 end
--- src/benchmarking.jl
+++ src/benchmarking.jl
@@ -8,11 +8,11 @@ using PProf
 # end
 
 scan = BreastScan{Float32, ComplexF32, UInt32}()
-domain_hemisphere!(scan, 2.5e-3, 7e-2+5e-3, Float32)
-load_scans!(scan,"data/B0_P3_p000.csv" , "data/B0_P3_p036.csv", ',', ComplexF32)
-load_frequencies!(scan, "data/frequencies.csv", ',', Float32)
-load_antennas!(scan, "data/antenna_locations.csv", ',', Float32)
-load_channels!(scan, "data/channel_names.csv", ',', UInt32)
+domain_hemisphere!(scan, 2.5e-3, 7e-2+5e-3)
+load_scans!(scan,"data/B0_P3_p000.csv" , "data/B0_P3_p036.csv", ',')
+load_frequencies!(scan, "data/frequencies.csv", ',')
+load_antennas!(scan, "data/antenna_locations.csv", ',')
+load_channels!(scan, "data/channel_names.csv", ',')
 scan.delayFunc = get_delays(Float32(8.0))
 scan.beamformerFunc = DAS
 image = abs.(beamform(scan))
